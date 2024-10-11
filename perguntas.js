0b8bd630248f5c8a344492998f04581df902819a
Bárbara Cristina <142352842+Ba-17@users.noreply.github.com>
2024-10-11 11:39:38 -0300

    u

---
+++ perguntas.js
@@ -0,0 +1,93 @@
+criaCartão(
+    'História',
+    'Quando ocoreu a Segunda Guerra Mundial?',
+    '1914-1918').
+    criaCartão(
+    'Química',
+    'Um carbono tretavalente faz quantas ligações e tem quantos elétrons na camada de valência?',
+    '4 ligações e 4ē CV').
+    criaCartão(
+    'Matemática',
+    'Qual é a fómula de bháskara?',
+    '-b ± √(b² - 4ac)/2a').
+    criaCartão(
+    'Química',
+    'Quais são as possíveis classificações de uma cadeia carbônica?',
+    'Acíclica, cíclica, aromatica, alicíclica/não aromática, saturada, insaturada, homogênea, heterogênea, normal e ramicada').
+    criaCartão(
+    'Educação Financeira',
+    'Cite 2 deveres do poder executivo na gestão pública.',
+    'Nomear e exonerar ministros de Estado, decretar estado de defesa e estado de sitio.').
+    criaCartão(
+    'História',
+    'Qual foi a criação de Getulio Vargas que possibilitou o mesmo a ficar no poder por causa do queremismo da população?',
+    'A criaçâo da CLT (Consolidação das Leis Trabalhistas)').
+    criaCartão(
+    'Geografia',
+    'O que é a ONU e qual a sua função?',
+    'Organização das Nações Unidas tem como função evitar novos conflitos globais'
+    ).
+    criaCartão(
+    'Geografia',
+    'Quais são as regiões do planeta que tem maior concentração de industrias?',
+    'Ásia, América, Oceania e Europa.')
+    criaCartão(
+    'Português',
+    'Qual foi o primeiro livro do Realismo no mundo?',
+    'Madame Bovary de Gustave Fleubert'
+    ).
+    criaCartão(
+    'História',
+    'O que foi a política "Café com Leite"?',
+    'Foi a alternância de candidatos das áreas de São Paulo e Minas Gerais'
+    ).
+    criaCartão(
+    'Biologia',
+    'Qual era a teoria de Darwin?',
+    'A teoria da Evolução'
+    ).
+    criaCartão(
+    'Biologia',
+    'Qual a teoria de Lamarck?',
+    'Teoria do Lamarckismo'
+    ).
+    criaCartão(
+    'Biologia',
+    'Qual a teoria de Mendel?',
+    'Principio da Segregação dos Caracteres'
+    ).
+    criaCartão(
+    'História',
+    'O que foi o dia do "Fico"?',
+    'Foi quando D.Pedro I desobedeceu seu pai D.João que o convocou para voltar a Portugal'
+    ).
+    criaCartão(
+    'Educação Financeira',
+    'Quais são os 5 princípios do Orçamento Público?',
+    'Legalidade, moralidade, eficiência, impessoalidade e publicidade'
+    ).
+    criaCartão(
+    'Educação Financeira',
+    'Quais são as Leis Orçamentarias?',
+    'PPA: Plano Plurianual, LDO: Lei de Diretrizes Orçamentarias e LOA: Lei Orçamentaria Anual'
+    ).
+    criaCartão(
+    'Educação Financeira',
+    'O que é União Europeia?',
+    'União economica e política entre 27 países principalmente europeus'
+    ).
+    criaCartão(
+    'Geografia',
+    'O que a Geopolítica faz?',
+    'Cuida das relações geograficas, acontecimentos históricos e políticos'
+    ).
+    criaCartão(
+    'Geografia',
+    'Quais problemas ambientais são causados pela concentração industrial?',
+    'Poluição, falta de sabeamento básico, aumento da violência e favelização'
+    ).
+    criaCartão(
+    'Empreendedorismo',
+    'O que são ferramentas de tomada de decisão?',
+    'São ferramentas que ajudam empreendedores ou empresas a tomar decisões no seus negócios'
+    )
