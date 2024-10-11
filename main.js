5f09028a51b8f1733c2b41df997f3d7984616512
Bárbara Cristina <142352842+Ba-17@users.noreply.github.com>
2024-10-11 12:01:18 -0300

    f

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function criaCartão(categoria, pergunta, resposta){
-    let container = document.getElementById(container');
+    let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.inner.HTML= `
